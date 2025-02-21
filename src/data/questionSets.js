6482e513bb4a06bbffa77be2ae716b56ecce2afb
James Perks <james.perks33@gmail.com>
2025-02-21 15:32:44 +1300

    Add initial question sets

---
+++ src/data/questionSets.js
@@ -0,0 +1,19 @@
+export const questionSets = {
+  basics: [
+    {
+      id: 'name',
+      question: "What's your name?",
+      category: 'basics'
+    },
+    {
+      id: 'birth',
+      question: "When and where were you born?",
+      category: 'basics'
+    },
+    {
+      id: 'childhood',
+      question: "What's your earliest childhood memory?",
+      category: 'basics'
+    }
+  ]
+};
